3f2f252a2330ad6a1f5a009471f36da3de1401e7
Colin A. White <nyquist212@yahoo.com>
2015-06-21 20:04:19 -0500

    index.html commentary

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function drawStreamSteps(error, data) {
 
         divwidth    = $('#streams').width();
 
-        margin      = { top: 20, right: 80, bottom: 20, left: 30 };
+        margin      = { top: 20, right: 50, bottom: 20, left: 30 };
 
         width       = divwidth - margin.left - margin.right;
 
@@ -119,7 +119,7 @@ function drawStreamSteps(error, data) {
                 .style("position", "absolute")
                 .style("z-index", "20")
                 .style("visibility", "hidden")
-                .style("top", "250px")
+                .style("top", "150px")
                 .style("left", (divwidth / 2) + "px");
 
         strokecolor = '#FFF';
@@ -175,7 +175,7 @@ function drawStreamSteps(error, data) {
                 .style("z-index", "19")
                 .style("width", "1px")
                 .style("height", divheight)
-                .style("top", "175px")
+                .style("top", "100px")
                 .style("bottom", "0px")
                 .style("left", "0px")
                 .style("background", "#fff");
@@ -203,7 +203,7 @@ function drawStreamSleep(error, data) {
 
         divwidth    = $('#streams').width(),
 
-        margin      = { top: 20, right: 80, bottom: 20, left: 30 };
+        margin      = { top: 20, right: 50, bottom: 20, left: 30 };
 
         width       = divwidth - margin.left - margin.right;
 
@@ -301,7 +301,7 @@ function drawStreamSleep(error, data) {
                 .style("position", "absolute")
                 .style("z-index", "20")
                 .style("visibility", "hidden")
-                .style("top", "250px")
+                .style("top", "150px")
                 .style("left", (divwidth / 2) + "px");
 
         strokecolor = '#FFF';
@@ -357,7 +357,7 @@ function drawStreamSleep(error, data) {
                 .style("z-index", "19")
                 .style("width", "1px")
                 .style("height", divheight)
-                .style("top", "175px")
+                .style("top", "100px")
                 .style("bottom", "-175px")
                 .style("left", "0px")
                 .style("background", "#fff");
@@ -373,7 +373,7 @@ function drawStreamSleep(error, data) {
                 });
 
 
-} // End draw sleep stram 
+} // End draw sleep strame
 
 /*  Helper Func populates and pads missing dates & data */
 
@@ -501,15 +501,38 @@ var parse = function(data) {
 
 d3.json("data/horizon-steps.json", function(error, data) {
 
-        // Override default color scale
-        color = d3.scale.linear()
-            .domain([-1, 0, 0, 1])
-            .range(["##BDCB87", "#DDD696", "#E29517", "#FF3300"]);
+        // Override default color scale in horizon.js
+        color       = d3.scale.linear().domain([-1, 0, 0, 1]) 
+                        .range(["##BDCB87", "#DDD696", "#E29517", "#FF3300"]);
 
-        margin = { top: 5, right: 5, bottom: 5, left: 5 };
-        height = 75 - margin.top - margin.bottom;
-        width = (document.getElementById("horizon-steps").offsetWidth) - margin.left - margin.right;
-        barPadding = 1;
+        margin      = { top: 5, right: 5, bottom: 5, left: 5 };
+
+        height      = 75 - margin.top - margin.bottom;
+        
+        width       = (document.getElementById("horizon-steps").offsetWidth) - margin.left - margin.right;
+        
+        barPadding  = 1;
+
+
+        xScale      = d3.time.scale()
+                        .domain(d3.extent(data, function(d) { return d.date; }))
+                        .range([0, width]);
+
+        yScale      = d3.scale.linear()
+                        .range([height, 0]);
+
+        horizon_stp = d3.horizon()
+                        .width(width)
+                        .height(height)
+                        .bands(4)
+                        .mode("offset")
+                        .interpolate("basis");
+
+        svg_hor_stp = d3.select("#horizon-steps").append("svg")
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         data.forEach(function(d) {
                 parseDate = d3.time.format("%Y-%m-%d").parse;
@@ -517,39 +540,13 @@ d3.json("data/horizon-steps.json", function(error, data) {
                 d.value = Math.round(+d.value);
         });
 
-        xScale = d3.time.scale()
-                .domain(d3.extent(data, function(d) {
-                        return d.date;
-                }))
-                .range([0, width]);
+        mean    = Math.round(d3.mean(data, function(d) { return d.value; }));
 
-        yScale = d3.scale.linear()
-                .range([height, 0]);
-
-        horizon_stp = d3.horizon()
-                .width(width)
-                .height(height)
-                .bands(4)
-                .mode("offset")
-                .interpolate("basis");
-
-        svg_hor_stp = d3.select("#horizon-steps").append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        mean = Math.round(d3.mean(data, function(d) {
-                return d.value;
-        }));
         // center data on the mean        
-        data.forEach(function(d) {
-                d.value = d.value - mean;
-        });
+        data.forEach(function(d) { d.value = d.value - mean; });
+
         // Convert date from UTC to Unix Epoch time
-        data = data.map(function(obj) {
-                return [obj.date.getTime(), obj.value];
-        });
+        data = data.map(function(obj) { return [obj.date.getTime(), obj.value]; });
 
         svg_hor_stp
                 .data([data])
@@ -584,55 +581,52 @@ d3.json("data/horizon-steps.json", function(error, data) {
 });
 
 
-
-
 d3.json("data/horizon-sleep.json", function(error, data) {
 
         // Override default color scale
-        color = d3.scale.linear()
-            .domain([-1, 0, 0, 1])
-            .range(["#AAC6C2", "#2D87AE", "#1C61A3", "#453485"]);
+        color       = d3.scale.linear().domain([-1, 0, 0, 1])
+                        .range(["#AAC6C2", "#2D87AE", "#1C61A3", "#453485"]);
 
-        margin = { top: 5, right: 5, bottom: 5, left: 5 };
-        height = 75 - margin.top - margin.bottom;
-        width = (document.getElementById("horizon-sleep").offsetWidth) - margin.left - margin.right;
-        barPadding = 1;
+        margin      = { top: 5, right: 5, bottom: 5, left: 5 };
+        
+        height      = 75 - margin.top - margin.bottom;
+        
+        width       = (document.getElementById("horizon-sleep").offsetWidth) - margin.left - margin.right;
+        
+        barPadding  = 1;
 
+
+
+        xScale      = d3.time.scale()
+                        .domain(d3.extent(data, function(d) { return d.date; }))
+                        .range([0, width]);
+
+        yScale      = d3.scale.linear().range([height, 0]);
+
+        horizon_slp = d3.horizon()
+                        .width(width)
+                        .height(height)
+                        .bands(4)
+                        .mode("offset")
+                        .interpolate("basis");
+
+        svg_hor_slp = d3.select("#horizon-sleep").append("svg")
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        
         data.forEach(function(d) {
                 parseDate = d3.time.format("%Y-%m-%d").parse;
                 d.date = parseDate(d.date);
                 d.value = Math.round(+d.value);
         });
 
-        xScale = d3.time.scale()
-                .domain(d3.extent(data, function(d) {
-                        return d.date;
-                }))
-                .range([0, width]);
+        mean = Math.round(d3.mean(data, function(d) { return d.value; }));
 
-        yScale = d3.scale.linear()
-                .range([height, 0]);
-
-        horizon_slp = d3.horizon()
-                .width(width)
-                .height(height)
-                .bands(4)
-                .mode("offset")
-                .interpolate("basis");
-
-        svg_hor_slp = d3.select("#horizon-sleep").append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        mean = Math.round(d3.mean(data, function(d) {
-                return d.value;
-        }));
         // center data on the mean        
-        data.forEach(function(d) {
-                d.value = d.value - mean;
-        });
+        data.forEach(function(d) { d.value = d.value - mean; });
+
         // Convert date from UTC to Unix Epoch time
         data = data.map(function(obj) {
                 return [obj.date.getTime(), obj.value];
@@ -669,4 +663,266 @@ d3.json("data/horizon-sleep.json", function(error, data) {
                 .call(hor_xAxis)
 
 });
+
+
+
+/***************************************/ 
+/*      SECTION 4 BAR CHARTS           */
+/***************************************/
+
+d3.json("data/cfilt-steps.json", function(d) {
+
+    d.forEach(function(d) {
+        parseDate = d3.time.format("%Y-%m-%d").parse;
+        d.date = parseDate(d.date); d.value = +d.value;
+        });
+    
+    mNames_stp  = ["January", "February", "March", "April", "May", "June", "July", 
+                    "August", "September", "October", "November", "December" ],
+
+    dNames_stp  = ["Sundays", "Mondays", "Tuesdays", "Wednesdays", "Thursdays", 
+                    "Fridays", "Saturdays" ];
+
+    margin      = {top:5, right:50, bottom: 40, left:5};
+    
+    height      = 125 - margin.top - margin.bottom; 
+    
+    width       = 500 - margin.left - margin.right; 
+    
+    barPadding  = 1;
+
+    steps       = crossfilter(d);
+    
+    monthdim    = steps.dimension(function(d){ thisDate = new Date(d.date); return thisDate.getMonth(); });
+    
+    mgrp_stp    = monthdim.group().reduceSum(function(d){ return d.value; });
+    
+    daydim      = steps.dimension(function(d){ thisDate = new Date(d.date); return thisDate.getDay(); });
+    
+    dgrp_stp    = daydim.group().reduceSum( function(d) { return d.value; });
+
+    mYscale_stp = d3.scale.linear()
+        .domain([0, d3.max(mgrp_stp.all(), function(d){return d.value;})])
+        .range([height, 5]);
+
+    dYscale_stp= d3.scale.linear()
+        .domain([0, d3.max(dgrp_stp.all(), function(d){return d.value;})])
+        .range([height, 5]);
+
+    dColor_stp = d3.scale.threshold() // Daily color scale
+        .domain([500000, 550000, 580000, 600000, 700000, 900000, 1300000])
+        .range(["#E3E3E3", "#D0DFD2", "#C3DABC", "#BDCB87", "#CAB44E", "#E29517", "#FF6600"]);
+
+    mColor_stp = d3.scale.threshold() // Monthly color scale
+        .domain([100000, 200000, 300000, 400000, 500000, 600000, 900000])
+        .range(["#E3E3E3", "#D0DFD2", "#C3DABC", "#BDCB87", "#CAB44E", "#E29517", "#FF6600"]);
+
+    d3.select("#monthly-steps-selector")
+        .on("click", function(d) { // Monthly Event listener
+            svg_steps.selectAll("rect.stepbars")
+                .remove();
+            svg_steps.selectAll("text.steptext")
+                .remove();
+            svg_steps.selectAll("rect.stepbars")
+                .attr("d", reDrawStepBars(mgrp_stp.all(), mYscale_stp, mColor_stp) );
+            svg_steps.selectAll("text.steptext")
+                .attr("d", reDrawStepText(mgrp_stp.all(), mYscale_stp, mNames_stp) );
+        });
+
+    d3.select("#daily-steps-selector")
+        .on("click", function(d) { // Daily Event listener 
+            svg_steps.selectAll("rect.stepbars")
+                .remove();
+            svg_steps.selectAll("text.steptext")
+                .remove();
+            svg_steps.selectAll("rect.stepbars")
+                .attr("d", reDrawStepBars(dgrp_stp.all(), dYscale_stp, dColor_stp) );
+            svg_steps.selectAll("text.steptext")
+                .attr("d", reDrawStepText(dgrp_stp.all(), dYscale_stp, dNames_stp) );
+        });
+
+    svg_steps = d3.select("#steps-bar")
+        .append("svg:svg")
+        .attr("width", width)
+        .attr("height", height);
+    
+    svg_steps.selectAll("rect.stepbars")
+        .data(mgrp_stp.all())
+        .enter().append("rect")
+        .attr("class", "stepbars")
+        .attr("x", function(d,i){ return i * width / mgrp_stp.size(); })
+        .attr("y", function(d){ return mYscale_stp(d.value); })
+        .attr("width", width / mgrp_stp.size() - barPadding)
+        .attr("height", function(d) { return height - mYscale_stp(d.value); })
+        .attr("fill", function(d){ return mColor_stp(d.value); });
+
+    svg_steps.selectAll("text.steptext")
+        .data(mgrp_stp.all())
+        .enter().append("text")
+        .attr("class", "steptext")
+        .attr("x", function(d,i){ return i* (width/mgrp_stp.size()) + (width/mgrp_stp.size() - barPadding) /2 ; })
+        .attr("y", function(d){ return mYscale_stp(d.value) + 15 })
+        .attr("text-anchor", "middle")
+        .attr("font-size", "10px")
+        .attr("fill", "white")
+        .html(function(d){ return mNames_stp[d.key].substring(0,1); })
+
+    function reDrawStepBars(data, scale, colorscale) {
+
+        svg_steps.selectAll("rect.stepbars")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "stepbars")
+            .attr("x", function(d,i){ return i * width / data.length; })
+            .attr("y", function(d){ return scale(d.value); })
+            .attr("width", width / data.length - barPadding)
+            .attr("height", function(d) { return height - scale(d.value); })
+            .attr("fill", function(d){ console.log(d.value) ; return colorscale(d.value); });
+    }; 
+
+    function reDrawStepText(data, scale, names) {
+   
+        svg_steps.selectAll("text.steptext")
+            .data(data)
+            .enter().append("text")
+            .attr("class", "steptext")
+            .attr("x", function(d,i){ return i* (width/data.length) + (width/data.length - barPadding) /2 ; })
+            .attr("y", function(d){ return scale(d.value) + 15 })
+            .attr("text-anchor", "middle")
+            .attr("font-size", "10px")
+            .attr("fill", "white")
+            .html(function(d){ return names[d.key].substring(0,1); });
+
+    };
+
+});
+
+d3.json("data/cfilt-sleep.json", function(d) {
+
+    d.forEach(function(d) {
+        parseDate = d3.time.format("%Y-%m-%d").parse;
+        d.date = parseDate(d.date); d.value = +d.value;
+        });
+    
+    mNames      = ["January", "February", "March", "April", "May", "June", "July", 
+                    "August", "September", "October", "November", "December" ],
+
+    dNames      = ["Sundays", "Mondays", "Tuesdays", "Wednesdays", "Thursdays", 
+                    "Fridays", "Saturdays" ];
+
+    margin      = {top:5, right:50, bottom: 40, left:5};
+    
+    height      = 125 - margin.top - margin.bottom; 
+    
+    width       = 500 - margin.left - margin.right; 
+    
+    barPadding  = 1;
+
+    sleep    = crossfilter(d);
+    
+    monthdim = sleep.dimension(function(d){ thisDate = new Date(d.date); return thisDate.getMonth(); });
+    
+    mgrp = monthdim.group().reduceSum(function(d){ return d.value; });
+    
+    daydim   = sleep.dimension(function(d){ thisDate = new Date(d.date); return thisDate.getDay(); });
+    
+    dgrp   = daydim.group().reduceSum( function(d) { return d.value; });
+
+    mYscale = d3.scale.linear()
+        .domain([0, d3.max(mgrp.all(), function(d){return d.value;})])
+        .range([height, 5]);
+
+    dYscale= d3.scale.linear()
+        .domain([0, d3.max(dgrp.all(), function(d){return d.value;})])
+        .range([height, 5]);
+
+    d3.select("#monthly-sleep-selector")
+        .on("click", function(d) { 
+            svg_sleep.selectAll("rect.sleepbars")
+                .remove();
+            svg_sleep.selectAll("text.sleeptext")
+                .remove();
+            svg_sleep.selectAll("rect.sleepbars")
+                .attr("d", redrawSleepBars(mgrp.all(), mYscale, mColor) );
+            svg_sleep.selectAll("text.sleeptext")
+                .attr("d", reDrawSleepText(mgrp.all(), mYscale, mNames) );
+        });
+
+    d3.select("#daily-sleep-selector")
+        .on("click", function(d) { 
+            svg_sleep.selectAll("rect.sleepbars")
+                .remove();
+            svg_sleep.selectAll("text.sleeptext")
+                .remove();
+            svg_sleep.selectAll("rect.sleepbars")
+                .attr("d", redrawSleepBars(dgrp.all(), dYscale, dColor) );
+
+            svg_sleep.selectAll("text.sleeptext")
+                .attr("d", reDrawSleepText(dgrp.all(), dYscale, dNames) );
+        });
+
+    dColor = d3.scale.threshold()
+        .domain([180, 190, 200, 210, 220, 230])
+        .range(["#E3E3E3", "#AAC6C2", "#6AA8B4", "#2D87AE", "#1C61A3E", "#453485"]);
+
+    mColor = d3.scale.threshold()
+        .domain([100, 200, 250, 300, 400, 500])
+        .range(["#E3E3E3", "#AAC6C2", "#6AA8B4", "#2D87AE", "#1C61A3E", "#453485"]);
+
+    svg_sleep = d3.select("#sleep-bar")
+        .append("svg:svg")
+        .attr("width", width)
+        .attr("height", height);
+    
+    svg_sleep.selectAll("rect.sleepbars")
+        .data(mgrp.all())
+        .enter().append("rect")
+        .attr("class", "sleepbars")
+        .attr("x", function(d,i){ return i * width / mgrp.size(); })
+        .attr("y", function(d){ return mYscale(d.value); })
+        .attr("width", width / mgrp.size() - barPadding)
+        .attr("height", function(d) { return height - mYscale(d.value); })
+        .attr("fill", function(d){ return mColor(d.value / 60); });
+
+    svg_sleep.selectAll("text.sleeptext")
+        .data(mgrp.all())
+        .enter().append("text")
+        .attr("class", "sleeptext")
+        .attr("x", function(d,i){ return i* (width/mgrp.size()) + (width/mgrp.size() - barPadding) /2 ; })
+        .attr("y", function(d){ return mYscale(d.value) + 15 })
+        .attr("text-anchor", "middle")
+        .attr("font-size", "10px")
+        .attr("fill", "white")
+        .html(function(d){ return mNames[d.key].substring(0,1); })
+
+    function redrawSleepBars(data, scale, colorscale) {
+
+        svg_sleep.selectAll("rect.sleepbars")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "sleepbars")
+            .attr("x", function(d,i){ return i * width / data.length; })
+            .attr("y", function(d){ return scale(d.value); })
+            .attr("width", width / data.length - barPadding)
+            .attr("height", function(d) { return height - scale(d.value); })
+            .attr("fill", function(d){ return colorscale(d.value / 60); });
+    }; 
+
+    function reDrawSleepText(data, scale, names) {
+   
+        svg_sleep.selectAll("text.sleeptext")
+            .data(data)
+            .enter().append("text")
+            .attr("class", "sleeptext")
+            .attr("x", function(d,i){ return i* (width/data.length) + (width/data.length - barPadding) /2 ; })
+            .attr("y", function(d){ return scale(d.value) + 15 })
+            .attr("text-anchor", "middle")
+            .attr("font-size", "10px")
+            .attr("fill", "white")
+            .html(function(d){ return names[d.key].substring(0,1); });
+
+    };
+
+});
+
 
